cd45f629caee5c8e39eea7f261847d724cf9aa1d
Geo Garcia <sr.pixelarmy@gmail.com>
2021-01-21 23:48:56 +0100

    Add project files.

---
+++ MainTarget.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ExceptionHandler
+{
+    partial class MainTarget
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Throw exception";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainTarget
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(138, 35);
+            this.Controls.Add(this.button1);
+            this.Name = "MainTarget";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainTarget_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MainTarget.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExceptionHandler
+{
+    public partial class MainTarget : Form
+    {
+       
+
+       
+           
+        public MainTarget()
+        {
+            InitializeComponent();
+        }
+
+        private void MainTarget_Load(object sender, EventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += (ae, eventArgs) =>
+            {
+                var Result = MessageBox.Show($"An exception has been thrown. Please report the following error message to a developer, the message has been copied to your clipboard:\n\n{((Exception)eventArgs.ExceptionObject)}");
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new Exception();
+        }
+    }
+}
